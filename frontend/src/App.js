27cb2770d768a889bcbb31c9b4337df8d32162b1
Gabriel Ionescu <ionescuig@yahoo.com>
2020-08-27 01:06:32 +0100

    navbar: include username (not finished)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,85 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter , Switch} from 'react-router-dom'
 import { Route  } from 'react-router-dom'
 import Login from './auth/Login'
 import Signup from './auth/Signup'
 import SongsList from './songs/SongsList'
 import SongCreateUpdate from './songs/SongCreateUpdate'
 import './App.css';
+import axiosInstance from "./auth/AuthServices";
 
-const BaseLayout = () => (
+
+class NavLayout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+    }
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentDidMount () {
+    try {
+      let username = localStorage.getItem("username")
+      this.setState({username: username})
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  handleLogout() {
+    this.setState({username: ""});
+    localStorage.removeItem('username');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    axiosInstance.defaults.headers['Authorization'] = null;
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand text-info" href="/">Django React Test</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Songs</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/songs">Create Song</a>
+            </li>
+          </ul>
+          {(() => {if (this.state.username) return (
+              <div className="form-inline my-2 my-lg-0">
+                <span className="navbar-text text-dark">Welcome <strong>{this.state.username}</strong></span>
+                <a className="nav-link" onClick={this.handleLogout} href="/">Logout</a>
+              </div>
+            );
+            else return (
+              <div className="form-inline my-2 my-lg-0">
+                <a className="nav-link" href="/signup/">Signup</a>
+                <a className="nav-link" href="/login/">Login</a>
+              </div>
+            )})()}
+        </div>
+      </nav>
+    )
+  }
+}
+
+const BaseLayout = ({username, signup}) => (
   <div className="container-fluid">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand text-info" href="/">Django React Test</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link" href="/">Songs</a>
-        <a className="nav-item nav-link" href="/songs">Create Song</a>
-      </div>
-      <div className="navbar-nav ml-auto">
-      <a className="nav-item nav-link" href="/signup/">Signup</a>
-        <a className="nav-item nav-link" href="/login/">Login</a>
-      </div>
-    </div>
-    </nav>
+    <NavLayout/>
     <div className="content">
-      <Route path="/" exact component={SongsList} />
-      <Route exact path={"/login/"} component={Login}/>
-      <Route exact path={"/signup/"} component={Signup}/>
-      <Route path="/songs/:id" component={SongCreateUpdate} />
-      <Route path="/songs/" exact component={SongCreateUpdate} />
+      <Switch>
+        <Route path="/" exact component={SongsList} />
+        <Route exact path={"/login/"} component={Login}/>
+        <Route exact path={"/signup/"} component={Signup}/>
+        <Route path="/songs/:id" component={SongCreateUpdate} />
+        <Route path="/songs/" exact component={SongCreateUpdate} />
+      </Switch>
     </div>
   </div>
 )
@@ -39,7 +88,7 @@ class App extends Component {
   render() {
     return (
     <BrowserRouter>
-      <BaseLayout/>
+      <BaseLayout />
     </BrowserRouter>
     );
   }
