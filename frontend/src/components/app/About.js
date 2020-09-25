4525b40895532b5eb97380fcc3b7166832a11ea7
Gabriel Ionescu <ionescuig@yahoo.com>
2020-09-25 14:06:59 +0100

    updated About Page

--- frontend/src/components/app/About.js
+++ frontend/src/components/app/About.js
@@ -7,40 +7,45 @@ class AboutPage extends React.Component {
 
     return (
       <div className="col-md-10 offset-md-1">
+        <div className="row">
 
-        {/* Errors */}
-        <div  style={{color: "red", margin: 40}}>
-          <div>
-            To Do:
+          <div className="col-md-8">
+            <h2 style={{marginBottom: 40}}>About Page</h2>
+            <p>Test Django REST Framework (API) and React.js (with Redux).</p>
             <ul>
-              <li>
-                Not geting songs list if anonymous user - check <strong>api</strong>.
-              </li>
-              <li>
-                Signup not done in <strong>api</strong> yet.
-              </li>
+              <li>user authentication</li>
+              <li>user authorisation</li>
+              <li>api calls</li>
+              <li>redux state</li>
+              <li>local storage</li>
+              <li>CRUD in backend</li>
+              <li>CRUD in frontend</li>
             </ul>
           </div>
-          <div><small>
-            Solved:
-            <ul>
-              <li>
-                Not updating on delete. Maybe because renders from state instead of props.
-              </li>
-              <li>
-                Not sorting. Maybe because can't modify props.  Maybe send as dispatch and modify in actions/reducers.
-              </li>
-              <li>
-                Create <strong>About</strong> page.
-              </li>
-              <li>
-                Move this list in <strong>About</strong> page.
-              </li>
-            </ul>
-          </small></div>
-        </div>
-      </div>
 
+
+          <div className="col-md-4" style={{color: "red"}}>
+            <div>
+              To Do:
+              <ul>
+                <li>Not geting songs list if anonymous user - check <strong>api</strong>.</li>
+                <li>Signup not done in <strong>api</strong> yet.</li>
+              </ul>
+            </div>
+
+            <div><small>
+              Solved:
+              <ul>
+                <li>Not updating on delete. Maybe because renders from state instead of props.</li>
+                <li>Not sorting. Maybe because can't modify props.  Maybe send as dispatch and modify in actions/reducers.</li>
+                <li>Create <strong>About</strong> page.</li>
+                <li>Move this list in <strong>About</strong> page.</li>
+              </ul>
+            </small></div>
+          </div>
+
+        </div>
+      </div>        
     )
   }
 }
